f5c98065f429d7adce3ee443f89697fa982e88c1
Sammi-code <54991360+Sammi-code@users.noreply.github.com>
2020-10-19 08:42:45 -0700

    More Updates

--- assets/js/userspage.js
+++ assets/js/userspage.js
@@ -93,17 +93,21 @@ function Notifications() {
     userDetails = " ";
     for (i = 0; i < userInfo.length; i++) {
         userDetails = `<div>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Beth+Ellen&family=Ubuntu&display=swap');
+        font-family:'Ubuntu',sans-serif;
+        </style>
 
-        <h3 style="font-family: Ubuntu; margin-top: 6rem; font-size: 18px; font-weight: bold;line-height: 23px;">Notification</h3>
-        <div style="height: 275px; width: 968px; left:37px; top:25px; display:flex;align-items:center;background: #686868;
+        <h3 style="margin-top: 6rem; font-size: 18px; font-weight: bold;line-height: 23px;">Notification</h3>
+        <div style="height: 245px; width: 850px; left:37px; top:25px; display:flex;align-items:center;background: #686868;
         background: #686868;box-shadow: 0px 0px 1.27907px rgba(0, 0, 0, 0.1), 0px 2.55814px 12.7907px rgba(0, 0, 0, 0.3);
 backdrop-filter: blur(55.63px);border-radius: 10px;">
 
             <div style=" position:relative;height: 225px;width: 228px; left: 37px; top: 70px;border-radius: 50%;"><img src="../assets/img/Woman Face.png"  onchange="loadFile(event)"  id="imageDisplay" style="position:relative;top:-50px;height: 173px; width: 161px;border-radius: 50%; border:12px rgba(248, 200, 0, 0.56) solid"> </div>
             <li name="string" id="string" style="display: none;"></li>
             <div style="position:relative;">
-                <p style="font-family:Ubuntu; height: 33px; width: 437px;font-size: 23px;font-style: normal;font-weight: 700;line-height: 27px;letter-spacing: 0px;text-align: left;height: 33px;color: #FFFFFF;">${userInfo[i].businessName} is live on NYC</p>
-                <h3 style="font-family: Ubuntu;font-style: normal;font-weight: 500; font-size: 18.0698px;  line-height: 21px;color: #FFFFFF;">Welcome. <br><br>All necessary and required messages and notifications would fall in here, promos and offers also.</h3>
+                <p style="height: 33px; width: 437px;font-size: 18px;font-style: normal; margin-left: 2rem; font-weight: normal;line-height: 27px;letter-spacing: 0px;text-align: left;height: 33px;color: #FFFFFF;">${userInfo[i].businessName} is live on NYC</p>
+                <h3 style="font-style: normal;font-weight: normal; font-size: 18px; margin-left: 2rem; line-height: 21px; color: #FFFFFF;">Welcome. <br><br>All necessary and required messages and notifications would fall in here, promos and offers also.</h3>
             </div>
         </div>
 
@@ -119,16 +123,20 @@ function ManageAds() {
     userDetails = " ";
     for (i = 0; i < userInfo.length; i++) {
         userDetails = `<div>
-
-        <h3 style="font-family: Ubuntu;  margin-top: 6rem; font-size: 18px;font-weight: bold;line-height: 23px;">Manage Ads</h3>
-        <div style="height: 275px; width: 968px; left:37px; top:25px; display:flex;align-items:center;background: #686868;
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Beth+Ellen&family=Ubuntu&display=swap');
+        font-family:'Ubuntu',sans-serif;
+        </style>
+        
+        <h3 style="  margin-top: 6rem; font-size: 18px;font-weight: bold;line-height: 23px;">Manage Ads</h3>
+        <div style="height: 245px; width: 850px; left:37px; top:25px; display:flex;align-items:center;background: #686868;
         background: #686868;box-shadow: 0px 0px 1.27907px rgba(0, 0, 0, 0.1), 0px 2.55814px 12.7907px rgba(0, 0, 0, 0.3);
 backdrop-filter: blur(55.63px);border-radius: 10px;">
 
             <div style=" position:relative;height: 225px;width: 228px; left: 37px; top: 70px;border-radius: 50%;"><img src="../assets/img/WomanFace2.png"  onchange="loadFile(event)"  id="imageDisplay" style="position:relative;top:-50px;height: 173px; width: 161px;border-radius: 50%; border:12px rgba(248, 200, 0, 0.56) solid"> </div>
             <li name="string" id="string" style="display: none;"></li>
             <div style="position:relative; top:-60px">
-            <p style= "font-family:ubuntu;color: #F8C800; font-size: 22px; font-weight: 500; line-height: 25px;">Information : <span style="color:#fff ">No ads found</span></p>
+            <p style= "font-family:ubuntu;color: #F8C800; font-size: 18px; font-weight: 500; line-height: 25px;">Information : <span style="color:#fff ">No ads found</span></p>
                   </div>
         </div>
 
@@ -137,8 +145,86 @@ backdrop-filter: blur(55.63px);border-radius: 10px;">
     document.getElementById("registration-form").innerHTML = userDetails
 }
 
+function addEvents(){
+    userDetails = " ";
+    for (i = 0; i < userInfo.length; i++) {
+        userDetails = `<div>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Beth+Ellen&family=Ubuntu&display=swap');
+        </style>
+        <h3 style="font-family: Ubuntu;  margin-top: 2rem; margin-bottom:1rem; font-size: 16px;line-height: 26px;">Events</h3>
+        <div style="height: 245px; width: 850px; left:37px; display:flex;align-items:center;background: #686868;
+        background: #686868;box-shadow: 0px 0px 1.27907px rgba(0, 0, 0, 0.1), 0px 2.55814px 12.7907px rgba(0, 0, 0, 0.3);
+backdrop-filter: blur(55.63px);border-radius: 10px;">
+
+            <div style=" position:relative;height: 200px;width: 228px; left: 37px; top: 70px;border-radius: 50%;"><img src="../assets/img/WomanFace2.png"  onchange="loadFile(event)"  id="imageDisplay" style="position:relative;top:-50px;height: 173px; width: 161px;border-radius: 50%; border:12px rgba(248, 200, 0, 0.56) solid"> </div>
+            <li name="string" id="string" style="display: none;"></li>
+            <div style="position:relative; top:-60px">
+            <p style= "font-family:'Ubuntu', sans-serif;color: #F8C800; font-size: 17px;  line-height: 25px;"> <span style="color:#fff; ">No events posted yet!</span></p> <br>
+            <button type="button" onclick="createEvents()" style="width:189px; height:53px; border:none; border-radius:10px; background-color:#F8C800; color:#000; box-shadow:#686868; font-weight:bolder;">Add Event</button>  
+            </div>
+        </div>
+    
+    </div>`
+}
+document.getElementById("registration-form").innerHTML = userDetails
+}
 
 
+
+function createEvents(){
+    userDetails = " ";
+    for (i = 0; i < userInfo.length; i++) {
+        userDetails = `<div>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Beth+Ellen&family=Ubuntu&display=swap');
+        font-family:'Ubuntu',sans-serif;
+        </style>
+        <h3 style="margin-top: 2rem; font-size: 16px;line-height: 26px;">Events</h3>
+        <h1 style="margin-top: 2.5rem; margin-bottom: 2.7rem; font-size: 20px;font-weight: bold;line-height: 26px; color:#000;">Create Event</h1>
+        <div>
+        <input style="width:400px; height:50px; margin-bottom: 1rem; border-radius:5px; color:#7B7B7B; font-size:15px; border:1px solid #C4C4C4;" type="text" name="name" id="name" placeholder="Event Name" required >
+        <select name="Location" id="location"   style="width:400px;  margin-bottom: 1rem; left:864px; height:50px; border-radius:5px; color:#7B7B7B; font-size:15px; border:1px solid #C4C4C4;">
+        <option value="location">&nbsp; Location</option>
+        <option value="lagos">Lagos</option>
+        <option value="abuja">Abuja</option>
+        <option value="ph">Porthacourt</option>
+        <option value="kaduna">Kaduna</option>
+        <option value="osun">Osun</option>
+        <option value="imo">Imo</option>
+        <option value="benin">Benin</option>
+     </select> <br>
+        <input style="width:400px; height:50px;  margin-bottom: 1rem; border-radius:5px; color:#7B7B7B; font-size:15px; border:1px solid #C4C4C4;" type="date" name="date" id="date" placeholder="Date" required >
+        <input type="text" name="file" id="file"  margin-bottom: 1rem; placeholder="Flyer/Poster/Picture" class="inputfile" style="width:282px;  margin-bottom: 1rem; height:50px; border-radius:5px; color:#7B7B7B; font-size:15px; border:1px solid #C4C4C4;" type="file" /> <button for="file" style="width:123px; margin-left:-10px; height:50px; border-radius:5px; background-color:#F8C800; color:#000; border:none; ">Choose a file</button><br>
+        <textarea style="width:400px; height:200px; padding:5px;  margin-bottom: 1rem; border-radius:5px; color:#7B7B7B; font-size:15px; border:1px solid #7B7B7B;" name="describe" id="event-description" placeholder="Event Description" required></textarea>
+            <button onclick= "goToDetails()" style="width:99px; height:53px; border-radius:10px; background-color:#F8C800; color:#000; border:none; font-weight:bolder;">Create</button>  
+            </div>
+        </div>
+    
+    </div>`
+}
+ document.getElementById("registration-form").innerHTML = userDetails
+}
+
+function goToDetails(){
+    userDetails = " ";
+    for (i = 0; i < userInfo.length; i++) {
+        userDetails = `<div>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Beth+Ellen&family=Ubuntu&display=swap');
+        font-family:'Ubuntu',sans-serif;
+        </style>
+        <h3 style="margin-top: 2rem; font-size: 16px;line-height: 26px;">Events</h3>
+               <div>
+              <img src="../assets/img/results.png" style="width: 850px; drop-shadow(10px 10px 30px rgba(174, 174, 192, 0.4)), drop-shadow(-10px -10px 30px #FFFFFF);"> <br> <br>
+              </div>
+                
+            <button type="button" onclick="createEvents()" style="width:160px; height:53px; border:none; border-radius:10px; background-color:#F8C800; color:#000; box-shadow:#686868; font-weight:bolder;">Add Event</button>  
+        </div>`
+}
+ document.getElementById("registration-form").innerHTML = userDetails
+
+}
 
 
 function details() {
@@ -186,7 +272,7 @@ function details() {
             <h2>Location <span class="glyphicion-box "></span></h2>
 
         </div>
-        <select name="Location " id="location" value="${userInfo[i].location}"class="custom-select-md  mb-3">
+        <select name="Location" id="location" value="${userInfo[i].location}" class="custom-select-md  mb-3">
             
                 <option value=" ">Location</option>
                 <option value="Abuja">Abuja</option>
@@ -200,7 +286,7 @@ function details() {
         <input type="text" value="${userInfo[i].location}" id="location" placeholder="location">
         <iframe src="../assets/img/Horizontal Map.png" style="display:block; width: 650px ;height:200px;" frameborder="0" scrolling="no"></iframe>
         <br>
-        <div class="buttons">
+        <div class="buttons ">
             <button onclick="update(${i})" class="btn btn-lg btn-warning">Update My Info</button>
         </div>
 
@@ -335,29 +421,85 @@ function usersbutton() {
 
 
 
+/*What was sent by Isiaka on saturday*/
 
-function getFile() {
+// function getFile() {
 
-    document.querySelector("#customFile").addEventListener("change", function() {
-        const reader = new FileReader()
+//     document.querySelector("#customFile").addEventListener("change", function() {
+//         const reader = new FileReader()
 
-        reader.addEventListener("load", () => {
+//         reader.addEventListener("load", () => {
 
-            localStorage.setItem("StoreImages", reader.result)
+//             localStorage.setItem("StoreImages", reader.result)
 
-        })
-        reader.readAsDataURL(this.files[0])
-    })
+//         })
+//         reader.readAsDataURL(this.files[0])
+//     })
 
 
+// }
+
+
+
+// function save() {
+//     let c = localStorage.getItem("StoreImages")
+//     document.getElementById("imageDisplay").src = c
+// }
+
+
+
+
+/* What i added */
+
+function getFile(e) {
+    var formInput = document.getElementById("customFile");
+    var filename = formInput.value;
+
+    var fr = new FileReader();
+    //fr.readAsBinaryString(formInput.files[0]);
+    fr.readAsDataURL(formInput.files[0]);
+    fr.onloadend = function(e) {
+        var string = e.target.result;
+        //  var liEle = document.getElementById("string")
+        //  liEle.innerHTML = bin;
+
+        var img = document.getElementById("imageDisplay");
+        img.src = string;
+
+
+    }
+    localStorage.setItem("userpics", JSON.stringify(string))
 }
 
 
 
-function save() {
-    let c = localStorage.getItem("StoreImages")
-    document.getElementById("imageDisplay").src = c
+function imgSave() {
+    var elephant = document.getElementById("imageDisplay");
+
+    // Take action when the image has loaded
+    elephant.addEventListener("load", function() {
+        var imgCanvas = document.createElement("canvas"),
+            imgContext = imgCanvas.getContext("2d");
+        // alert(imgContext)
+        // Make sure canvas is as big as the picture
+        imgCanvas.width = elephant.width;
+        imgCanvas.height = elephant.height;
+
+        // Draw image into canvas element
+        imgContext.drawImage(elephant, 0, 0, elephant.width, elephant.height);
+
+        // Get canvas contents as a data URL
+        var imgAsDataURL = imgCanvas.toDataURL("image/png/jpeg/jpg");
+
+        // Save image into localStorage
+        try {
+            localStorage.setItem("elephant", imgAsDataURL);
+        } catch (e) {
+            console.log("Storage failed: " + e);
+        }
+    }, false);
 }
+
 
 
 function statTab() {
@@ -377,6 +519,8 @@ function statTab() {
                         <img src="../assets/img/yellowchart" alt="">
                     </div>
                 </div>
+                <hr>
+                <hr>
                 <div class="bottom-container">
                     <div class="left-bottom">
                         <img src="/assets/img/Man thumb.png" alt=""> <br>
